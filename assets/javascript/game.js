c8ed7a7a942388635b0e0b3a10fb5266797e9e5d
koa2019 <no-reply>
2019-06-29 13:47:50 -0700

    got the click function to work. rewrote the loop that processes each character & changes wins & loses

---
+++ assets/javascript/game.js
@@ -0,0 +1,54 @@
+// if (remainingGuess > 0 && remainingGuess < 11) {
+                //     console.log(remainingGuess);
+                //     if (userInputUpper.indexOf(guessThisWord) !== -1) {
+                //         console.log(userInputUpper + " = " + guessThisWord);
+                //     }
+                //     else {
+                //         remainingGuess--;
+                //     }
+                //     currentWordText.textContent = userInputUpper;
+                // }
+
+                // //conditional to check if user won & guessed word correctly without running out of guesses            
+                // if (userInput === guessThisWord) {
+
+                //     wins++;
+                //     console.log("After if conditional # wins: " + win);
+                // }
+                // else {
+                //     remainingGuess--;
+                //     remainingGuessText.textContent = remainingGuess;
+
+                // }
+
+                // for (var i = 0; i < guessThisWord.length; i++) {
+                //     console.log("guessWord in loop: " + guessThisWord);
+                //     console.log("guessWord in loop: " + guessThisWord.length);
+
+                //     if (userInput === search(guessThisWord)) {
+
+                //         string1 = string1 + " " + guessThisWord[i];
+                //         currentWordText.textContent = string1;
+
+                //         console.log(guessThisWord);
+                //         console.log(guessThisWord[i]);
+                //         console.log(string1);
+                //     }
+
+                // }
+                // for (var i = 0; i < guessThisWord[i]; i++) {
+                //     if(userInput === guessThisWord[i]) {
+                //         currentWordText.textContent(userInput);
+                //         console.log(userInput);
+                //     }
+                // }
+                // }
+                // if (userInput !== guessThisWord || userInput !== letter) {
+                //     remainingGuess--;
+                //     wrongLetterString = userInput + " , " + wrongLetterString;
+                //     remainingGuessText.textContent = remainingGuess;
+                //     wrongLetterText.textContent = wrongLetterString;
+                //     console.log(remainingGuess);
+                //     console.log(wrongLetterString);
+                //     console.log(userInput);
+                // }
